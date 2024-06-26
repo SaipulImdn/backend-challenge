25dbe0262f7c83a732faf54730e4aab7ba81df8a
Syaiful Imanudin <yusriltc@gmail.com>
2024-06-26 13:13:58 +0700

    Update config.go

--- configs/config.go
+++ configs/config.go
@@ -3,25 +3,32 @@ package configs
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 var (
-    db  *gorm.DB
-    err error
+	db  *gorm.DB
+	err error
 )
 
 func ConnectDB() {
-    dsn := "host=localhost port=5433 user=postgres dbname=online_store password=saipul1452 sslmode=disable"
-    db, err = gorm.Open("postgres", dsn)
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println("Database connected!")
+	host := os.Getenv("POSTGRES_HOST")
+	port := os.Getenv("POSTGRES_PORT")
+	user := os.Getenv("POSTGRES_USER")
+	dbname := os.Getenv("POSTGRES_DB")
+	password := os.Getenv("POSTGRES_PASSWORD")
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable", host, port, user, dbname, password)
+	db, err = gorm.Open("postgres", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Database connected!")
 }
 
 func GetDB() *gorm.DB {
-    return db
+	return db
 }
